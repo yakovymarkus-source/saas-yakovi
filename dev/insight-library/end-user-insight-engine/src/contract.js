91523c96f0323e32fa10bebf835febf9c7799e49
yakov <yakovymarkus@gmail.com>
2026-04-20 18:58:39 +0300

    עדכון אוטומטי - 2026-04-20 18:58:38

--- dev/insight-library/end-user-insight-engine/src/contract.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-const CONTRACT_VERSION = '1.0.0';
-
-module.exports = { CONTRACT_VERSION };
--- dev/prototype/proj/src/middleware/asyncHandler.js
+++
@@ -1,7 +0,0 @@
-function asyncHandler(fn) {
-  return function wrappedAsyncHandler(req, res, next) {
-    Promise.resolve(fn(req, res, next)).catch(next);
-  };
-}
-
-module.exports = { asyncHandler };
